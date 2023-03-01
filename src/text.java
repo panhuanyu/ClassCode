c3815d23d334ca5bdd7f28bb018909cf6ae917bf
pan <panhuanyu@yunfang.com>
2023-03-01 17:11:40 +0800

    2023.3.1commit-2

--- src/text.java
+++ src/text.java
@@ -1,2 +1,13 @@
 public class text {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+        System.out.println("hello world1!");
+        System.out.println("hello world2!");
+        System.out.println("hello world3!");
+        System.out.println("hello world!4");
+        System.out.println("hello world!5");
+        System.out.println("hot-fix!5");
+        System.out.println("hot-fix!6");
+    }
 }
+
